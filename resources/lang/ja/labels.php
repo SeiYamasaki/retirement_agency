043622b7354f7fc84c085e992810c31d1b2a4175
seiis <seiisland@gmail.com>
2025-02-26 20:47:01 +0900

    ロケールja変更

---
+++ resources/lang/ja/labels.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'token' => 'トークン',
+    'conflict' => '競業禁止契約の有無',
+    'public_servant' => '公務員かどうか',
+    'licensed_professional' => '有資格者かどうか',
+    'contractor' => '契約社員かどうか',
+    'foreign_trainee' => '外国人技能実習生かどうか',
+    'mental_illness' => '精神疾患の有無',
+    'trouble' => 'トラブルの有無',
+    'name' => '氏名',
+    'name_furigana' => '氏名（フリガナ）',
+    'gender' => '性別',
+    'birth_date' => '生年月日',
+    'age' => '年齢',
+    'line_name' => 'LINE登録名',
+];
